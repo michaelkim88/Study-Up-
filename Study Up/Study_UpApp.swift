8ce626d4e3f3fe86926e74deba563ef203592fd1
Trevor Smith <91441655+8tsmith11@users.noreply.github.com>
2025-03-01 21:51:31 -0500

    Initial Commit

---
+++ Study Up/Study_UpApp.swift
@@ -0,0 +1,17 @@
+//
+//  Study_UpApp.swift
+//  Study Up
+//
+//  Created by Trevor Smith on 3/1/25.
+//
+
+import SwiftUI
+
+@main
+struct Study_UpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Study UpTests/Study_UpTests.swift
@@ -0,0 +1,17 @@
+//
+//  Study_UpTests.swift
+//  Study UpTests
+//
+//  Created by Trevor Smith on 3/1/25.
+//
+
+import Testing
+@testable import Study_Up
+
+struct Study_UpTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
