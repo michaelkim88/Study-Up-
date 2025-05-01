881c65c9cc2095606ea190c2faa6ea70d4db1b2d
michaelkim88 <makim3509@gmail.com>
2025-04-30 22:18:00 -0400

    Auto selects keyboard when new set

--- Study Up/Testing/testing.swift
+++ Study Up/Testing/testing.swift
@@ -1,53 +1,38 @@
 import SwiftUI
-
-struct ContentView: View {
-    
-    @State private var episodes: [Episode] = MockData.episodes
-    
+struct TextSheet: View {
+    @State var text = "Starting text"
+    @FocusState var showkeyboard :Bool
     var body: some View {
-        NavigationStack {
-            List ($episodes, editActions: .move) { episode in
-                HStack(alignment: .top, spacing: 12) {
-                    RoundedRectangle(cornerRadius: 12)
-                        .frame(width: 80, height: 80)
-                        .foregroundStyle(episode.color.wrappedValue)
-                    VStack(alignment: .leading) {
-                        Text ("Episode Title")
-                            .font(.headline)
-                        Text ("Here is the short description for the latest episode.")
-                            .font(.subheadline)
-                            .foregroundStyle(.secondary)
-                    }
-                }
+        TextEditor(text: $text).bold().font(.title)
+            .padding(12)
+            .focused($showkeyboard)
+            .navigationTitle ("Dynamic keyboard")
+            .onAppear (){
+                showkeyboard = true
             }
-            .navigationTitle("Episodes")
+    }
+}
+
+import SwiftUI
+struct ContentView: View {
+    @State var showView = false
+    var body: some View {
+        VStack {
+            Button (action: {
+                showView.toggle()
+            }, label: {
+                Label("Start typing", systemImage: "keyboard.fill")
+            })
+            .tint(.green)
+            .buttonStyle(.bordered)
+            .controlSize(.large)
         }
+        .padding()
+        .sheet(isPresented: $showView, content: {
+            TextSheet ()
+        })
     }
 }
-
-#Preview { NavigationStack { ContentView() } }
-
-class Episode: Identifiable {
-    let id = UUID()
-    var title: String
-    var color: Color
-    var listOrder: Int
-    
-    init(title: String, color: Color, listOrder: Int) {
-        self.title = title
-        self.color = color
-        self.listOrder = listOrder
-    }
-}
-
-struct MockData {
-    static var episodes: [Episode] {
-        [
-            Episode(title: "Pink Episode", color: .pink, listOrder: 0),
-            Episode (title: "Teal Episode", color: .teal, listOrder: 1),
-            Episode(title: "Indigo Episode", color: .indigo, listOrder: 2),
-            Episode(title: "Orange Episode", color: .orange, listOrder: 3),
-            Episode(title: "Green Episode", color: .green, listOrder: 4),
-            Episode(title: "Purple Episode", color: .purple, listOrder: 5),
-            Episode(title: "Yellow Episode", color: .yellow, listOrder: 6)]}
+#Preview {
+    ContentView()
 }
